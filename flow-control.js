f24ffa0967e618ff7d455eb8f1f0a3b08499a35c
Julien Vier <vier.julien@googlemail.com>
2020-01-04 10:29:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age) {
-  if (13 >= age || age <= 19){
+  if (13 >= age && age <= 19){
   return "You are a teenager!";
   }
 }
 
 function teenager(age) {
-  if (13 > age || age < 19) {
+  if (13 > age && age < 19) {
     return "You are a teenager!"
   }
   else {
